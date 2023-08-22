7da78357dbf5b4d526e953c105feaf349e42aa38
Pretham Muthappa <prethambachira1819@gmail.com>
2023-08-23 01:22:15 +0530

    routes

--- routes/store.js
+++ routes/store.js
@@ -7,3 +7,23 @@ const app=express();
 const publish=process.env.Publishable_key;
 app.use(express.json());
 const router=express.Router()
+
+router.post('/check',async(req,res)=>{
+	const{card,expiry,cvv}=req.body;
+
+	try{
+
+     const charge=await stripe.charges.create({
+
+		card:card,
+		expiry:expiry,
+		cvv:cvv
+	 })
+
+	 res.status(200).send('done')
+
+	}
+	catch(err){
+		res.status(500).send('errror')
+	}
+})
